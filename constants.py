c3e7c5c722acb69fc0d7228deadb8e078feafbde
Uhanalainen <loukaja@gmail.com>
2024-02-09 09:39:17 +0200

    Fix bugged Kaaoszine reviews. Add support for Soundi.
    Since Soundi release dates are formatted in only month/year
    format, it was necessary to add another constant variable for
    the month part so the reference looks good. Not thrilled with
    the solution but it'll work for now, maybe in the future one
    could use a single list and use title on it and add 'ta' to the
    end of the month if needed instead of having two separate lists.

--- constants.py
+++ constants.py
@@ -3,4 +3,5 @@ URL = 'https://auth.tidal.com/v1/oauth2/token'
 PARAMS = {'grant_type': 'client_credentials'}
 
 # Months
+KK_BASE = ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu']
 KK = ['tammikuuta', 'helmikuuta', 'maaliskuuta', 'huhtikuuta', 'toukokuuta', 'kesäkuuta', 'heinäkuuta', 'elokuuta', 'syyskuuta', 'lokakuuta', 'marraskuuta', 'joulukuuta']
