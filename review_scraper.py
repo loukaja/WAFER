7e900116ffb0fb01ad63b466bf6de27ce7f933fb
Uhanalainen <loukaja@gmail.com>
2024-02-09 14:54:51 +0200

    Add support for Blabbermouth reviews

--- review_scraper.py
+++ review_scraper.py
@@ -19,6 +19,8 @@ def get_review(review_url):
             review = get_kaaoszine_review(soup, review_url)
         elif domain == 'www.soundi.fi':
             review = get_soundi_review(soup, review_url)
+        elif domain == 'blabbermouth.net':
+            review = get_blabbermouth_review(soup, review_url)
     except requests.exceptions.Timeout:
         sys.exit("The request timed out")
     except requests.exceptions.RequestException as e:
@@ -27,7 +29,7 @@ def get_review(review_url):
     return review
 
 def create_album_rating(review):
-    if review['domain'] == 'kaaoszine.fi':
+    if review['domain'] == 'kaaoszine.fi' or review['domain'] == 'blabbermouth.net':
         site = review['domain'].split('.')[0].title()
     elif review['domain'] == 'www.soundi.fi':
         site = review['domain'].split('.')[1].title()
@@ -40,9 +42,16 @@ def create_reference(review):
     current_date = datetime.now().strftime("%d.%m.%Y")
     # Remove leading zeros from day and month
     current_date = current_date.replace('.0', '.').lstrip('0')
-    reference = (f"<ref>{{{{Verkkoviite | Osoite = {review['url']} | Nimeke = {review['title']} | "
+
+    if review['domain'] != 'blabbermouth.net':
+        reference = (f"<ref>{{{{Verkkoviite | Osoite = {review['url']} | Nimeke = {review['title']} | "
                  f"Tekijä = {review['author']} | Sivusto = {review['domain']} | "
                  f"Ajankohta = {review['date']} | Viitattu = {current_date} }}}}</ref>")
+    else:
+        reference = (f"<ref>{{{{Verkkoviite | Osoite = {review['url']} | Nimeke = {review['title']} | "
+                    f"Tekijä = {review['author']} | Sivusto = {review['domain']} | "
+                    f"Ajankohta = | Viitattu = {current_date} | Kieli = {{{{en}}}} }}}}</ref>")
+
     return reference
 
 def get_kaaoszine_review(soup, review_url):
@@ -130,7 +139,43 @@ def get_soundi_review(soup, review_url):
     
     album_rating = create_album_rating(review)
 
-    return album_rating 
+    return album_rating
+
+def get_blabbermouth_review(soup, review_url):
+    domain = 'blabbermouth.net'
+    artist = soup.find('h1', class_='margin__top-default margin__bottom-default').get_text()
+    album = soup.find('h2', class_='margin__bottom-default').get_text()
+
+    title = artist + ' - ' + album
+
+    # Get rating
+    rating_div = soup.find('div', class_='reviews-rate-comments margin__top-default margin__bottom-default')
+
+    if rating_div:
+        rating = rating_div.find('div').get_text().split('/')[0].strip()
+    else:
+        rating = None
+    max_rating = 10
+
+    # Find the div containing the author's name
+    author_div = soup.find('div', class_='news-relative-items').find('div')
+
+    # Extract the author's name from the text within the div
+    author_name = author_div.get_text(strip=True).replace('Author:', '')
+
+    review = {
+            "title": title,
+            "author": author_name,
+            "date": 'Unavailable',
+            "rating": rating,
+            "max_rating": max_rating,
+            "url": review_url,
+            "domain": domain
+        }
+
+    album_rating = create_album_rating(review)
+
+    return album_rating
 
 if __name__ == '__main__':
 
